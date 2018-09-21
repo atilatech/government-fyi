c502d1e63923d4fc517442211b04c523112256db
Jimmy Chion <ajimmykid@gmail.com>
2018-09-21 14:38:16 -0700

    prop 2 mostly

--- src/pages/2/expandables.js
+++ src/pages/2/expandables.js
@@ -1,24 +1,42 @@
-export const Acronyms = {
-
-}
+// export const Acronyms = {
+//
+// }
 
 export const Citations = {
   '1': {
-    link:"",
-    publication:"",
-    headline:"",
-    directQuote:"",
+    link:"http://www.latimes.com/politics/la-pol-ca-homeless-housing-bond-stalled-20180301-story.html",
+    publication:"LA Times",
+    headline:"$2 billion to help house California's homeless isn't being spent — and no one knows when it will be",
+    directQuote:"\"I recognize that this housing is needed, but stealing treatment money from the severely mentally ill is despicable and counterproductive,\" Bernard said. Some mental health advocates share Bernard's concern about the money being spent on a housing bond when needs for services remain high.\"It was a treatment-first law, not a housing-first law,\" said Rose King, who co-authored the 2004 initiative.",
   },
   '2': {
-    link:"",
-    publication:"",
-    headline:"",
-    directQuote:"",
+    link:"https://sd24.senate.ca.gov/news/2016-06-30-legislature-passes-%E2%80%9Cno-place-home%E2%80%9D-initiative-tackle-homelessness",
+    publication:"State Senate",
+    headline:"Legislature Passes “No Place Like Home” Initiative to Tackle Homelessness",
+    directQuote:"This proposal provides $2 billion for the construction and rehabilitation of permanent supportive housing for homeless individuals with mental illness.  The “No Place Like Home” initiative takes a “housing first” approach, which has been used successfully in other states and is considered best practice by many homeless advocates and social service experts.",
   },
   '3': {
-    link:"",
-    publication:"",
-    headline:"",
-    directQuote:"",
+    link:"http://www.latimes.com/politics/la-pol-ca-homeless-housing-bond-stalled-20180301-story.html",
+    publication:"LA Times",
+    headline:"$2 billion to help house California's homeless isn't being spent — and no one knows when it will be",
+    directQuote:"But litigation stalled the effort. Mary Ann Bernard, an attorney from Sacramento, said the proposal is illegal. The California Constitution typically requires voter approval for all bond measures, and Proposition 63 didn't specify financing housing construction as one of the ways money could be spent.",
+  },
+  '4': {
+    link:"http://www.sandiegouniontribune.com/opinion/editorials/sd-proposition-two-help-homeless-menatl-health-utak-20180917-story.html",
+    publication:"San Diego Union-Tribune",
+    headline:"Editorial | Proposition 2: Vote yes for a smart way to help the homeless",
+    directQuote:"It would authorize the state to use Proposition 63 revenue — about $120 million a year — to pay off $2 billion in new revenue bonds for homelessness prevention housing for those in need of mental health services.",
+  },
+  '5': {
+    link:"https://www.sacbee.com/news/politics-government/capitol-alert/article202396749.html",
+    publication: "Sac Bee",
+    headline: "Counties slow to spend millionaires' money on mental health",
+    directQuote:"California State Auditor Elaine Howle found that county mental health programs had stashed $231 million from the tax that should have been returned to the state by the end of the 2015-16 budget year."
+  },
+  '6': {
+    link:"http://www.latimes.com/politics/la-pol-ca-homeless-housing-bond-stalled-20180301-story.html",
+    publication:"LA Times",
+    headline:"$2 billion to help house California's homeless isn't being spent — and no one knows when it will be",
+    directQuote:"Hundreds of millions of dollars in treatment money could be diverted toward interest and administrative payments to support the bond, Bernard said. She also believes that cities and counties are often reluctant to approve homeless housing development, leading to fewer new homes funded by bond money than proponents suggest."
   },
 }
