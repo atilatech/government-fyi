cca555cdeb0baeb088fa46e037b6c3c43c91bd81
yvonneleow <yvonnetleow@gmail.com>
2018-10-06 12:10:03 -0700

    home page intro

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -11,16 +11,16 @@ let Data = {
 	meta: {
 		canonicalUrl:"https://www.ballot.fyi/",
 		documentTitle:"California State Propositions – ballot.fyi",
-		socialHeadline:"The quickest nonpartisan voter guide to this November",
-		socialDescription:"11 CA propositions including daylight savings, cage-free eggs, and housing",
+		socialHeadline:"The quickest nonpartisan voter guide for the midterms",
+		socialDescription:"Learn about 11 CA propositions, including daylight saving, cage-free eggs, and rent control",
 		socialImage:"general.png",
 	},
 	blocks: [
 		{
 			component: FrontPageTitleBlock,
 			data: {
-				title: "Nonpartisan guide to California's 2018 election",
-				body: <span> We're tired of constant news about Trump. Luckily, California gives us 11 semi-ridiculous ballot initiatives to vote on and make fun of. We'll break them down for you, what the different sides are saying, and why your vote is actually important. <br/><br/>This is still a work in progress, but you can stay in touch by <Link to="/follow">subscribing</Link> to our newsletter. And! please, for fuck's sake, make sure you're <a target="_blank" rel="noopener noreferrer" href="https://www.vote.org/">registered to vote</a> for November 6, 2018.</span>,
+				title: "A Nonpartisan guide to California's 2018 election",
+				body: <span> We're tired of the constant news about Trump. Luckily, California gives us 11 semi-ridiculous ballot initiatives to vote on and make fun of. We'll break them down for you, explain what the different sides are saying and why your vote is actually important. <br/><br/>We'll be updating our guide up until Election Day on November 6th, but <Link to="/follow">subscribe</Link> to our newsletter to stay in touch. And please, for fuck's sake, make sure you're <a target="_blank" rel="noopener noreferrer" href="https://www.vote.org/">registered to vote</a>.</span>,
 			}
 		}
   ]
