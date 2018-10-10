7fb5fcce7eeabe43ebf47aed4781ab51655ec2c7
Jimmy Chion <ajimmykid@gmail.com>
2018-10-09 23:47:48 -0700

    adds ordering to homepage

---
+++ src/components/page-blocks/sections/PropSection.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import styled from 'styled-components'
+import {PropMetaData} from 'pages/prop-attributes'
+import PropCardBlock from 'components/page-blocks/blocks/PropCardBlock'
+import Color from 'layout/colors'
+
+const ButtonLabel = styled.h2`
+  font-size: 16px;
+`
+const Button = styled.div`
+  padding: 5px 10px;
+  background-color: ${props=>props.color};
+  color: white;
+  width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  margin-left: 20px;
+  box-shadow: 2px 3px 2px rgba(0,0,0,0.1);
+  border-radius: 2px;
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+`
+class PropSection extends React.Component {
+  constructor(props) {
+    super(props)
+    this.ordering = {
+      spicy: [7,10,12,5,6,11,9,3,8,2,1,4],
+      numerical: [1,2,3,4,5,6,7,8,9,10,11,12]
+    }
+    this.state={ordering: this.ordering.spicy}
+  }
+
+  changeOrder = (arr) => {
+    this.setState({ordering:arr})
+  }
+
+  render() {
+    const orderingSelection =
+      <ButtonContainer>
+        <h3>Order the props by</h3>
+        <Button
+          color={Color('pink2')}
+          onClick={() => this.changeOrder(this.ordering.spicy)}>
+          <ButtonLabel>Spiciness</ButtonLabel>
+        </Button>
+        <Button
+          color="#444"
+          onClick={() => this.changeOrder(this.ordering.numerical)}>
+          <ButtonLabel>Number</ButtonLabel>
+        </Button>
+      </ButtonContainer>
+
+    const propsInOrder = this.state.ordering.map( (propNum, i) => {
+      const {color, imageSet, title, socialDescription} = PropMetaData(propNum)
+      return(
+        <PropCardBlock
+          key={i}
+          data = {{
+            propNum: propNum + "",
+            header: title,
+            color: color,
+            description: socialDescription,
+            img: imageSet,
+            linksTo: `/prop-${propNum}`,
+          }}
+        />
+      );
+  	});
+
+    return(
+      <React.Fragment>
+        {orderingSelection}
+        {propsInOrder}
+      </React.Fragment>
+    );
+  }
+}
+export default PropSection;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PageBuilder from 'components/page-blocks/aggregators/PageBuilder'
 import FrontPageTitleBlock from 'components/page-blocks/blocks/FrontPageTitleBlock'
-import PropCardBlock from 'components/page-blocks/blocks/PropCardBlock'
+// import PropCardBlock from 'components/page-blocks/blocks/PropCardBlock'
+import PropSection from 'components/page-blocks/sections/PropSection'
 import {Link} from 'react-router-dom'
 import {PropMetaData} from 'pages/prop-attributes'
 // import CAImg from './images/ca.png'
+
 
 let Data = {
 	pageId:"ballot-fyi-props",
@@ -22,24 +24,16 @@ let Data = {
 				title: "A Nonpartisan guide to California's 2018 election",
 				body: <span> We're tired of the constant news about Trump. Luckily, California gives us 11 semi-ridiculous ballot initiatives to vote on and make fun of. We'll break them down for you, explain what different sides are saying and why your vote actually matters. <br/><br/>We'll be updating our guide up until Election Day on November 6th, but <Link to="/follow">subscribe</Link> to our newsletter to stay in touch. And please, for fuck's sake, make sure you're <a target="_blank" rel="noopener noreferrer" href="https://www.vote.org/">registered to vote</a>.</span>,
 			}
+		},
+		{
+			component: PropSection,
+			data:{}
 		}
   ]
 }
 
-for (let i = 1; i < 13; i++){
-	const {color, imageSet, title, socialDescription} = PropMetaData(i)
-	Data.blocks.push({
-		component: PropCardBlock,
-		data: {
-			propNum: i + "",
-			header: title,
-			color: color,
-			description: socialDescription,
-			img: imageSet,
-			linksTo: `/prop-${i}`,
-		}
-	});
-}
 
 
-export default () => (<PageBuilder data={Data}/>);
+export default () => (
+	<PageBuilder data={Data}/>
+);
