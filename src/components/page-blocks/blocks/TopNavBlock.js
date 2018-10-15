3de92043717a092c7a4594611f00aa03cd3206cd
Jimmy Chion <ajimmykid@gmail.com>
2018-10-15 10:36:54 -0700

    progress

--- src/components/page-blocks/blocks/TopNavBlock.js
+++ src/components/page-blocks/blocks/TopNavBlock.js
@@ -9,22 +9,30 @@ import {PropMetaData} from 'pages/prop-attributes'
 
 const Img = styled(MultisourceImage)`
 	display: block;
-  ${'' /* filter: drop-shadow(12px 12px 0 rgba(0,0,0,0.15)) drop-shadow(-20px 16px 2px rgba(0,0,0,0.05)) ; */}
-	height: 70px;
+	height: 60px;
+	width: 60px;
 	object-fit: contain;
+	filter: grayscale(100%);
 `;
 
 const Banner = styled.div`
   width: 100%;
-  height: 70px;
 	display: flex;
-	justify-content: space-between;
+	justify-content: space-around;
+	flex-wrap: wrap;
 	align-items: center;
   background-color: whitesmoke;
+	padding: 10px 0;
 `
 const NavItemContainer = styled.div`
-	max-width: 100px;
-	background-color: ${props=>Color(props.color+'2')};
+	transition: background-color 150ms ease-out;
+	background-color: ${props=>props.isActive?Color(props.color+'1'):'transparent'};
+	border-radius: 35px;
+	@media not all and (hover: none) {
+		&:hover{
+			background-color: ${props=>Color(props.color+'2')};
+		}
+	}
 `
 
 const TopNavBlock = (props) => {
@@ -43,7 +51,7 @@ const TopNavBlock = (props) => {
 	const navItems = navArr.map( (item, i) => {
 		return(
 			<Link to={`/prop-${i+1}`} key={i}>
-				<NavItemContainer color={item.color}>
+				<NavItemContainer color={item.color} isActive={props.currentProp===i+1}>
 					<Img imageHandles={{_1x:item.images._1x, _1x_webp:item.images._1x_webp}} alt={item.desc}/>
 				</NavItemContainer>
 			</Link>
