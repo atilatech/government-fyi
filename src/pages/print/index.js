515c67020e76cd27f6bef2f4aee5dd605e3bb323
Jimmy Chion <ajimmykid@gmail.com>
2018-10-22 00:51:08 -0700

    finalizes print page

--- src/pages/print/index.js
+++ src/pages/print/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 import PageBuilder from 'components/page-blocks/aggregators/PageBuilder'
 import PageTitleBlock from 'components/page-blocks/blocks/PageTitleBlock'
-import ImgBlock from 'components/page-blocks/blocks/ImgBlock'
+import ImgAndTitleBlock from 'components/page-blocks/blocks/ImgAndTitleBlock'
 import CustomBlock from 'components/page-blocks/blocks/CustomBlock'
 import SectionTitleBlock from 'components/page-blocks/blocks/SectionTitleBlock'
 import TextWithTitleBlock from 'components/page-blocks/blocks/TextWithTitleBlock'
 import BookletImg from './images/printguide.jpg'
+import BookletImg_2x from './images/printguide@2x.jpg'
+import BookletImg_w from './images/printguide.webp'
+import BookletImg_2x_w from './images/printguide@2x.webp'
 import Acronym from 'components/interactive/acronym'
 import MultisourceImg from 'components/static/multisource-image'
 
@@ -14,15 +17,7 @@ import CA_front from './images/ca-cover.png'
 import CA_front_w from './images/ca-cover.webp'
 import CA_front_2x from './images/ca-cover@2x.png'
 import CA_front_2x_w from './images/ca-cover@2x.webp'
-import CA_back from './images/ca-back.png'
-import CA_back_w from './images/ca-back.webp'
-import CA_back_2x from './images/ca-back@2x.png'
-import CA_back_2x_w from './images/ca-back@2x.webp'
 
-import SF_back from './images/sf-back.png'
-import SF_back_w from './images/sf-back.webp'
-import SF_back_2x from './images/sf-back@2x.png'
-import SF_back_2x_w from './images/sf-back@2x.webp'
 import SF_front from './images/sf-cover.png'
 import SF_front_w from './images/sf-cover.webp'
 import SF_front_2x from './images/sf-cover@2x.png'
@@ -33,27 +28,46 @@ const Container = styled.div`
   justify-content: center;
   margin-top: 30px;
 `
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const DownloadLink = styled.a`
   display: block;
   background-color: #eee;
-  width: 200px;
+  height: 300px;
   border: 1px solid #ccc;
   margin: 0 30px;
   filter: drop-shadow(12px 12px 0 rgba(0,0,0,0.10)) drop-shadow(-20px 16px 2px rgba(0,0,0,0.05));
-  transition: transform 200ms ease-in;
-  ${'' /* @media not all and (hover: none) {
-    &:hover{
-      transform: rotate3d(0,1,0,180deg);
-
-    }
-  } */}
 `
 const Image = styled(MultisourceImg)`
   display: block;
   width: 100%;
   object-fit: contain;
 `
+const ImgLabel = styled.h3`
+  margin-top: 20px;
+`
+
+const DownloadBox = (props) => {
+  const links = props.links.map( (link, i) => (
+    <LinkContainer>
+      <DownloadLink href={link.slug} download>
+        <Image
+          imageHandles={link.imageHandles}
+          alt={link.alt}/>
+      </DownloadLink>
+      <ImgLabel>{link.label}</ImgLabel>
+    </LinkContainer>
+  ))
+  return (
+    <Container>
+      {links}
+    </Container>
+  )
+}
 
 const Data = {
 	pageId:"subscribe-page",
@@ -66,70 +80,65 @@ const Data = {
 	},
 	blocks: [
   {
-    	component: PageTitleBlock,
+    component: ImgAndTitleBlock,
     data: {
-    		title: "Download our print guide",
-    }
-  },
-  {
-      component: ImgBlock,
-    data: {
-      src: {
-          _1x: BookletImg,
-          // _1x_webp: partnerlogos_w,
-      },
-        alt: "Image of printed voter guide",
-        caption: "We blurred the background to make this image look cooler.",
-        nColWidth: 6,
-    }
-  },
-  {
-      component: TextWithTitleBlock,
-    data:{
-        title:"Who designed this beautiful thing?",
-        text:
+      title: "Download the print guide",
+      subtitle:
       <span>
-        With the content from our site, our friends at <a target="_blank" rel="noopener noreferrer" href="http://www.work-work-work.com/">Working Hard</a> designed us a beautiful print voter guide. We have two books available for download as <Acronym expanded="Portable Document Format" acronym="PDF"/>s: the California and San Francisco elections. They're designed to be printed on standard 8.5" x 11" letter paper, but you do you.
+        With content from our site, our friends at <a target="_blank" rel="noopener noreferrer" href="http://www.work-work-work.com/">Working Hard</a> designed us a beautiful print voter guide. We have two books available for your download as <Acronym expanded="Portable Document Format" acronym="PDF"/>s: the California and San Francisco elections. They're designed to be printed on standard 8.5" x 11" letter paper, but you do you.
 
         <br/><br/>Please keep in mind that the information in the books were last updated in early October, because printing. Please refer to this very website 👇 and <a href="https://www.bythebay.cool">By The Bay</a> for the most up-to-date information.
-      </span>
+      </span>,
+      src: {
+          _1x: BookletImg,
+          _1x_webp: BookletImg_w,
+          _2x: BookletImg_2x,
+          _2x_webp: BookletImg_2x_w,
+      },
+        alt: "Image of printed voter guide",
+        caption: "Don't you just want to rub these against your face, lovingly? (Photo provided by Working Hard)",
+        nColWidth: 7,
     }
   },
   {
-      component: SectionTitleBlock,
+    component: SectionTitleBlock,
     data: {
-        nColWidth: 6,
-        title:"Download",
-        subtitle:"By clicking the images"
+      nColWidth: 6,
+      title:"Click to download them",
+      paddingTop: 20,
     }
   },
   {
-      component: CustomBlock,
+    component: CustomBlock,
     data:{
-        nColWidth: 6,
+        nColWidth: 8,
         body:
-      <Container>
-        <DownloadLink href="https://www.ballot.fyi/downloads/ballotfyi-CA-voter-guide.pdf" download>
-          <Image
-            imageHandles={{
-                _1x: CA_back,
-                _1x_webp: CA_back_w,
-                _2x: CA_back_2x,
-                _2x_webp: CA_back_2x_w
-            }}
-          alt="Click to download CA print guide"/>
-        </DownloadLink>
-        <DownloadLink href="https://www.ballot.fyi/downloads/ballotfyi-SF-voter-guide.pdf" download>
-          <Image
-            imageHandles={{
+        <DownloadBox
+          links={[
+            {
+              slug:"/downloads/ballotfyi-CA-voter-guide.pdf",
+              imageHandles:{
+                _1x: CA_front,
+                _1x_webp: CA_front_w,
+                _2x: CA_front_2x,
+                _2x_webp: CA_front_2x_w
+              },
+              alt:"Click to download CA print guide",
+              label:"California"
+            },
+            {
+              slug:"/downloads/ballotfyi-SF-voter-guide.pdf",
+              imageHandles:{
                 _1x: SF_front,
                 _1x_webp: SF_front_w,
                 _2x: SF_front_2x,
                 _2x_webp: SF_front_2x_w
-            }}
-          alt="Click to download SF print guide"/>
-          </DownloadLink>
-        </Container>
+              },
+              alt:"Click to download SF print guide",
+              label:"San Francisco"
+            },
+          ]}
+        />
       }
     },
   ]
