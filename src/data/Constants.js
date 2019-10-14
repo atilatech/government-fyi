d3382f80f9c997cb40be940d37ae883784e08d0c
ademidun <tomiademidun@gmail.com>
2019-10-14 18:13:59 -0400

    added ENTREPRENEURS_AND_SMALL_BUSINESS and unemployed to ALL_DEMOGRAPHICS

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -6,6 +6,8 @@ export const SENIORS = 'seniors';
 export const IMMIGRANTS = 'immigrants';
 export const HOMEOWNERS = 'homeowners';
 export const OIL_WORKERS = 'oil workers';
+export const ENTREPRENEURS_AND_SMALL_BUSINESS = 'entrepreneurs and small business';
+export const UNEMPLOYED = 'unemployed';
 export const FOREIGNERS = 'foreigners';
 export const LOW_INCOME = 'low income';
 export const WOMEN = 'women';
@@ -14,4 +16,5 @@ export const VISIBLE_MINORITY = 'visible minority';
 export const ALL_DEMOGRAPHICS = [
     RECENT_GRADS, YOUNG_PEOPLE, STUDENTS,SENIORS,
     CLIMATE_CONSCIOUS, IMMIGRANTS, HOMEOWNERS, OIL_WORKERS,
+    ENTREPRENEURS_AND_SMALL_BUSINESS, UNEMPLOYED,
     FOREIGNERS, LOW_INCOME, WOMEN, VISIBLE_MINORITY];
