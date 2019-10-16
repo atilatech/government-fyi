2d17faf835ff73be3b6282307a0179220a2eaa47
ademidun <tomiademidun@gmail.com>
2019-10-16 13:38:30 -0400

    remove unused import in home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,21 +1,6 @@
 import React from 'react'
-// import styled from 'styled-components'
 import PageBuilder from 'components/page-blocks/aggregators/PageBuilder'
-// import TextWithTitleBlock from 'components/page-blocks/blocks/TextWithTitleBlock'
-import PropSection from 'components/page-blocks/sections/PropSection'
 import LandingPage from "../../scenes/LandingPage/LandingPage";
-// import {Link} from 'react-router-dom'
-// import CAImg from './images/ca.png'
-// import {Button} from 'pages/about'
-//
-// const Attrib = styled.h3`
-//   display: block;
-//   font-size: 11px;
-//   color: #999;
-//   margin-bottom: 20px
-//   text-align: center;
-// `
-
 
 let Data = {
 	pageId:"home-page",
