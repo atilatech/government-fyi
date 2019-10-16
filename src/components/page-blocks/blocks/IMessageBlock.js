7325a9af6a17d754f87407c4bf2b925eb430d071
ademidun <tomiademidun@gmail.com>
2019-10-15 21:17:38 -0400

    added PartyPlatformDataEducation and toggle to show more or less of IMessage conversations

--- src/components/page-blocks/blocks/IMessageBlock.js
+++ src/components/page-blocks/blocks/IMessageBlock.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Row, Col } from 'react-flexbox-grid';
 import {FromThem, FromMe, Clear, Section, FromThemEmoji} from '../../static/iMessageStyles'
+import {Button} from "antd";
 
 /*
 an iMessage conversation block
@@ -32,56 +33,99 @@ example usage for data.js
 	},
 },
 */
+const DEFAULT_MESSAGES_TO_SHOW = 7;
 
-const IMessageBlock = (props) => {
-	const {messages} = props.data;
-	let lastSender = null;
-	const conversation = messages.map( (message, i) => {
-		let conditionalClear = null;
-		let msg = null;
-		if (lastSender !== message.from) {
-			conditionalClear = <Clear/>
-		}
-		lastSender = message.from;
+class IMessageBlock extends React.Component {
 
-		switch(message.from) {
-			case "them":
-				msg = <FromThem>{message.body}</FromThem>
-				break;
-			case "me":
-				msg = <FromMe>{message.body}</FromMe>
-				break;
-			case "me-no-bg":
-				msg = <FromMe style={{background:'none'}}>{message.body}</FromMe>
-				break;
-			case "them-no-bg":
-				msg = <FromThemEmoji>{message.body}</FromThemEmoji>
-				break;
-			default:
-				break;
+	constructor(props) {
+		super(props);
+
+		const {messages} = this.props.data;
+
+		this.state = {
+			hideMessages: messages.length > DEFAULT_MESSAGES_TO_SHOW,
 		}
-		return (
-			<React.Fragment key={i}>
-				{conditionalClear}
-				{msg}
-				<br />
+	}
+	showHideMoreMessagesToggle = () => {
+
+		const { hideMessages } = this.state;
+
+		this.setState({hideMessages : !hideMessages});
+
+	};
+
+	render ( ) {
+
+		let {messages} = this.props.data;
+		let {hideMessages} = this.state;
+
+		if (hideMessages) {
+			messages = messages.slice(0, DEFAULT_MESSAGES_TO_SHOW);
+		}
+
+
+		let lastSender = null;
+		const conversation = messages.map( (message, i) => {
+			let conditionalClear = null;
+			let msg = null;
+			if (lastSender !== message.from) {
+				conditionalClear = <Clear/>
+			}
+			lastSender = message.from;
+
+			switch(message.from) {
+				case "them":
+					msg = <FromThem>{message.body}</FromThem>
+					break;
+				case "me":
+					msg = <FromMe>{message.body}</FromMe>
+					break;
+				case "me-no-bg":
+					msg = <FromMe style={{background:'none'}}>{message.body}</FromMe>
+					break;
+				case "them-no-bg":
+					msg = <FromThemEmoji>{message.body}</FromThemEmoji>
+					break;
+				default:
+					break;
+			}
+			return (
+				<React.Fragment key={i}>
+					{conditionalClear}
+					{msg}
+					<br />
+				</React.Fragment>
+			)
+		})
+		return(
+			<React.Fragment>
+			<Row>
+				<Col
+					xsOffset={1} xs={10}
+					smOffset={2} sm={8}
+					mdOffset={3} md={6}
+					lgOffset={3} lg={6}
+				>
+					<Section>
+						{conversation}
+					</Section>
+				</Col>
+			</Row>
+			<Row className="mt-3">
+				<Col
+					xsOffset={1} xs={10}
+					smOffset={2} sm={8}
+					mdOffset={3} md={6}
+					lgOffset={3} lg={6}
+				>
+				<Button type="primary" onClick={this.showHideMoreMessagesToggle}>
+					Show {hideMessages ? 'More' : 'Less'} Messages
+				</Button>
+				</Col>
+			</Row>
 			</React.Fragment>
-		)
-	})
-	return(
-		<Row>
-	    <Col
-	    	xsOffset={1} xs={10}
-	      smOffset={2} sm={8}
-	      mdOffset={3} md={6}
-	      lgOffset={3} lg={6}
-	    >
-				<Section>
-					{conversation}
-				</Section>
-			</Col>
-		</Row>
-	);
+		);
+	}
 }
 
 IMessageBlock.propTypes = {
--- src/pages/4/index.js
+++ src/pages/4/index.js
@@ -11,6 +11,7 @@ import Citation from '../../components/interactive/citation/citation'
 import Acronym from '../../components/interactive/acronym'
 import {Acronyms, Citations} from './expandables'
 import {EmojiLg} from "../../components/static/iMessageStyles";
+import {EDUCATION_CONVERSATION_MESSAGES} from "../../data/PartyPlatformDataEducation";
 
 const Data = {
 	pageId:"education",
@@ -30,122 +31,7 @@ const Data = {
 		{
 			component: IMessageBlock,
 			data: {
-				messages: [
-					{
-						from: "me",
-						body: <span>I'm graduating soon </span>,
-					},
-					{
-						from:"me-no-bg",
-						body:<EmojiLg><span role="img" aria-label="crying emoji">😭</span></EmojiLg>
-					},
-					{
-						from: "them",
-						body: "You should be celebrating. Why the crying face?",
-					},
-					{
-						from: "me",
-						body: "I realized that I'm graduating with over $20,000 in student loans"
-					},
-					{
-						from: "me",
-						body: "And I have to start paying it back 6 months after I graduate."
-					},
-					{
-						from: "me",
-						body: "Also, I don't have a job lined up for when I leave school."
-					},
-					{
-						from: "them",
-						body: "Well it could be worse"
-					},
-					{
-						from: "them",
-						body: <span>
-							Most people graduate with about
-							<Citation data={Citations['7']}>$28,00 in student loans</Citation>.
-						</span>
-					},
-					{
-						from: "me",
-						body: "Not helping 🙄"
-					},
-					{
-						from: "them",
-						body: "Look on the bright side, I heard that some parties were talking about forgiving student loans"
-					},
-					{
-						from: "me",
-						body: "Forgiving student loans? "
-					},
-					{
-						from: "me",
-						body: "I like the sound of that 😁"
-					},
-					{
-						from:"me-no-bg",
-						body:<EmojiLg><span role="img" aria-label="thinking emoji">🤔</span></EmojiLg>
-					},
-					{
-						from: "me",
-						body: "But Wait, what about the people that worked hard to pay their student loans"
-					},
-					{
-						from: "them",
-						body: "Unfortunately, we can't change the past."
-					},
-					{
-						from: "them",
-						body: "The most important thing now is making sure future students " +
-							"aren't crushed with debt when they graduate."
-					},
-					{
-						from: "me",
-						body: "hmm, I see what you mean"
-					},
-					{
-						from: "me",
-						body: "But what happens the next year. Back to square one?"
-					},
-					{
-						from: "me",
-						body: "Why not just make university tuition free?"
-					},
-					{
-						from: "them",
-						body: <span>chill, that would be way too expensive.
-
-							You have the <Citation data={Citations['7']}>$10.6 billion</Citation> to pay for that?
-						</span>
-					},
-					{
-						from: "them",
-						body: "You can't even afford your $20,000 student loan . 😂"
-					},
-					{
-						from:"me-no-bg",
-						body:<EmojiLg><span role="img" aria-label="angry emoji">😡</span></EmojiLg>
-					},
-					{
-						from: "them",
-						body: "Sorry, I got carried away 😂"
-					},
-					{
-						from: "them",
-						body: <span>
-							You have a point though. I mean <Citation data={Citations['7']}>Germany has free tuition.</Citation>
-							Even for international students.
-						</span>
-					},
-					{
-						from: "me",
-						body: "btw...One more thing..."
-					},
-					{
-						from: "me",
-						body: "Can I have a job? 😁"
-					},
-				]
+				messages: EDUCATION_CONVERSATION_MESSAGES,
 			}
 		},
     {
