4e1e5631103ef15429869c03c8c942cbdd2bb5c6
ademidun <tomiademidun@gmail.com>
2021-09-11 19:37:53 -0400

    put maclean's links first because it's the most comprehensive

--- src/data/References.js
+++ src/data/References.js
@@ -1,18 +1,5 @@
 export const References = [
     {
-        subsectionTitle: "Business News Network and CBC",
-        links: [
-            {
-                text: "2021 federal election platform tracker: Where each party stands so far (BNN)",
-                url: "https://www.bnnbloomberg.ca/2021-federal-election-platform-tracker-where-each-party-stands-so-far-1.1639592"
-            },
-            {
-                text: "How do the main parties compare on these issues? (CBC)",
-                url: "https://newsinteractives.cbc.ca/elections/federal/2021/party-platforms/"
-            }
-        ]
-    },
-    {
         subsectionTitle: "Maclean's and CTV",
         links: [
             {
@@ -23,6 +10,19 @@ export const References = [
                 text: "2019 Federal election platforms (CTV)",
                 url: "https://election.ctvnews.ca/platforms"
             },
+        ]
+    },
+    {
+        subsectionTitle: "Business News Network and CBC",
+        links: [
+            {
+                text: "2021 federal election platform tracker: Where each party stands so far (BNN)",
+                url: "https://www.bnnbloomberg.ca/2021-federal-election-platform-tracker-where-each-party-stands-so-far-1.1639592"
+            },
+            {
+                text: "How do the main parties compare on these issues? (CBC)",
+                url: "https://newsinteractives.cbc.ca/elections/federal/2021/party-platforms/"
+            }
         ]
     },
 ];
